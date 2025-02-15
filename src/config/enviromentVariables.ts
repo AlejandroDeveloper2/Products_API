12e8e92e4ce4fa7fed71afcd7f309b98dd97c168
AlejandroDeveloper2 <90485793+AlejandroDeveloper2@users.noreply.github.com>
2025-02-15 15:42:22 -0500

    Implementación de middleware de auth para proteger rutas

--- src/config/enviromentVariables.ts
+++ src/config/enviromentVariables.ts
@@ -2,4 +2,5 @@ import "dotenv/config";
 
 export const enviromentVariables = {
   SERVER_PORT: process.env.SERVER_PORT || 3001,
+  AUTH_API_URL: process.env.AUTH_API_URL || "",
 };
--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,17 +1,23 @@
 import { Router } from "express";
 
 import ProductController from "@controllers/Product.controller";
+import authMiddleware from "@middleware/auth.middleware";
 
 export const ProductsRouter = Router();
 
 const productController = new ProductController();
 
-ProductsRouter.post("/products", productController.postProduct)
-  .get("/products/:productId", productController.getProductById)
-  .get("/products", productController.getProducts)
+ProductsRouter.post("/products", authMiddleware, productController.postProduct)
+  .get("/products/:productId", authMiddleware, productController.getProductById)
+  .get("/products", authMiddleware, productController.getProducts)
   .get(
     "/products/category/:productCategory",
+    authMiddleware,
     productController.getProductsByCategory
   )
-  .put("/products/:productId", productController.putProduct)
-  .delete("/products/:productId", productController.deleteProductById);
+  .put("/products/:productId", authMiddleware, productController.putProduct)
+  .delete(
+    "/products/:productId",
+    authMiddleware,
+    productController.deleteProductById
+  );
